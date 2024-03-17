80f87109b7a84b0f5877091fa968a9cee113e06b
Becxster <beccstah5@gmail.com>
2024-03-16 21:29:40 -0500

    Podcast section, pop-ups (WIP)

--- js/slides.js
+++ js/slides.js
@@ -1,25 +1,4 @@
 
-// Sticky Navbar
-
-// When the user scrolls the page, execute myFunction
-window.onload = function() {navStick()};
-window.onscroll = function() {navStick()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function navStick() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
 // Parallax
 
 let colorfulDotsL = document.getElementById('colorfulDots-l')
